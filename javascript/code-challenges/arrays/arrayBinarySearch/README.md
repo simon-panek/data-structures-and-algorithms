7b10e20206b339309cff0e06c051d638d11fc6e7
Simon Panek <paneks19@gmail.com>
2020-12-10 00:00:06 -0800

    update readme and code now working

--- javascript/code-challenges/arrays/arrayBinarySearch/README.md
+++ javascript/code-challenges/arrays/arrayBinarySearch/README.md
@@ -8,4 +8,4 @@ Given an ordered array of values and a key value, use binary search to output th
 Our solution involves splitting the array in half and checking the first half of the index positions for a match with the key. If found it returns the index position. If not, it recursively runs the same logic on the remaining index positions. When the key is found its current index is added to the sum of all previous sub arrays. If the length of the final sub array is one and the key is not found it returns -1 to indicate that it is not in the array.
 
 ## Solution
-![Whiteboard Solution](401-code-challenge-day3-array-binary-search.png)
+![Whiteboard Solution](401-code-challenge-day3-array-binary-search-2.png)
--- javascript/code-challenges/arrays/arrayBinarySearch/array-binary-search.js
+++ javascript/code-challenges/arrays/arrayBinarySearch/array-binary-search.js
@@ -1,50 +1,46 @@
 'use strict';
 
-
-function binarySearch (inputArray, searchValue) {
-
+function binarySearch(inputArray, searchValue) {
   let loopIndex = 0;
   let foundIndex = -2;
   let splitIndex = 0;
   let initialArrayLength = inputArray.length;
   let arrayLength = inputArray.length;
-  
-  if (!inputArray) {
-    let foundIndex = -1
-  };
-  
-  function findNewSplitIndex (inputArrayLength, oldSplitIndex) {
-    if (inputArrayLength % 2 === 1) { 
-      splitIndex = (inputArrayLength / 2 ) + 0.5;
-    } else { 
-      splitIndex = inputArray / 2;
+  if (!inputArray) { // Check if the array exists (not undefined)
+    //foundIndex = -1;
+    return -1;
+  } else if (inputArray == null) {
+    //foundIndex = -1;
+    return -1;
+  }
+  let findNewSplitIndex = (inputArrayLength, oldSplitIndex) => {
+    if (inputArrayLength % 2 === 1) {
+      splitIndex = (inputArrayLength / 2) + 0.5; // if true: odd number handling
+    } else if (inputArray.length % 2 === 0) {
+      splitIndex = (inputArrayLength / 2); // if false: even number handling
     }
     splitIndex += oldSplitIndex;
     loopSplitFunction(oldSplitIndex, splitIndex);
-  }
-
-  function loopSplitFunction (loopIndex, splitIndex) {
-    for ( let i = loopIndex; i < splitIndex; i ++) {
-      if ( inputArray[i] === searchValue ) {
-        foundIndex = i;
-      } else {
-        arrayLength = initialArrayLength - splitIndex;
-        if ( arrayLength = 1 ) {
-          return foundIndex = initialArrayLength;
-        }
-        loopIndex = splitIndex;
-      }
+  };
+  let loopSplitFunction = (loopIndex, splitIndex) => {
+    for(loopIndex; loopIndex < splitIndex; loopIndex++) {
+      if (inputArray[loopIndex] === searchValue) {
+        foundIndex = loopIndex; // We found our value!
+      } else if (loopIndex >= initialArrayLength) { // If it wasn't found in this sub-search
+      foundIndex = -1;
     }
   }
-
-  if ( foundIndex > -1 ) {
-    return foundIndex;
-  }  else if ( initialArrayLength = loopIndex ) {
-    return foundIndex = -1;
-  } else {
-    findNewSplitIndex(arrayLength, loopIndex);
+  // Loop over, we didn't find anything... our new array length is total - split index
+  arrayLength = initialArrayLength - splitIndex;
+  if (arrayLength === 0) {
+    foundIndex = -1; // End of array. It wasn't found at all.
+  } else if (foundIndex === -2) {
+    findNewSplitIndex(arrayLength, splitIndex);
   }
-
+};
+if (foundIndex === -2) { // Causes the initial run on the sub-functions and edge cases
+  findNewSplitIndex(arrayLength, loopIndex); // Begin searching
 }
-
-binarySearch ([1,2,3,4,5,6,7,8,9,10], 7);
+return foundIndex;
+}
+binarySearch(initialArray, searchValue);
