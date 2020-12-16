4ce9fc4c5f74a65c4e29532290cb9c30032f9ebb
Simon Panek <paneks19@gmail.com>
2020-12-16 14:45:47 -0800

    complete cc8 all except writing test

--- javascript/code-challenges/linked-list/linkedList/index.js
+++ javascript/code-challenges/linked-list/linkedList/index.js
@@ -103,6 +103,34 @@ class LinkedList { //creates LinkedList class
     return nodeValue[k];
   }
 
+  llZip(givenListOne, givenListTwo) {
+
+    if(givenListOne === null && givenListTwo !== null) { return givenListTwo };
+    if(givenListOne !== null && givenListTwo === null) { return givenListOne };
+    if(givenListOne === null && givenListTwo === null) { return false };
+
+    let currentNodeOne = givenListOne.head;
+    let currentNodeTwo = givenListTwo.head;
+
+    while (givenListOne !== null && givenListTwo !== null) {
+      if(currentNodeOne.next !== null && currentNodeTwo !== null) { 
+        currentNodeTwo.next = currentNodeOne.next;
+        currentNodeOne.next = currentNodeTwo;
+      };
+      if(currentNodeOne.next === null) {
+        currentNodeOne = currentNodeTwo.next;
+      }
+      if(currentNodeTwo.next === null) {
+        currentNodeOne = currentNodeOne.next;
+      }
+      currentNodeOne = currentNodeOne.next;
+      currentNodeTwo = currentNodeTwo.next;
+    }
+
+    return givenListOne;
+
+  }
+
 }
 
 module.exports = LinkedList;
