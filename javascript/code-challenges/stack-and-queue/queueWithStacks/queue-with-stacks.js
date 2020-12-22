be6881942483e2f13142032809989c1dad8f6116
Simon Panek <paneks19@gmail.com>
2020-12-22 07:56:40 -0800

    tests passing

--- javascript/code-challenges/stack-and-queue/queueWithStacks/queue-with-stacks.js
+++ javascript/code-challenges/stack-and-queue/queueWithStacks/queue-with-stacks.js
@@ -4,57 +4,59 @@ const Stack = require ('../stacks.js');
 
 class PseudoQueue {
   constructor() {
-    // this.stackA = new Stack();
-    // this.stackB = new Stack();
-    this.stackA = [];
-    this.stackB = [];
+    this.stackA = new Stack();
+    this.stackB = new Stack();
+    // this.stackA = [];
+    // this.stackB = [];
    // this.front
     //this.end
   }
 
-    enqueue(value) {
-      this.stackA.push(value);
-    }
+    // enqueue(value) {
+    //   this.stackA.push(value);
+    // }
 
-    dequeue(){
-      while(this.stackA){
-        let item = stackA.pop();
-        this.stackB.push(item)
+    // dequeue(){
+    //   while(this.stackA){
+    //     let item = stackA.pop();
+    //     this.stackB.push(item)
+    //   }
+    //   return this.stackB.pop();
+    // }
+  enqueue(value) {
+
+    if (this.stackB.length > 0){
+      let stackBLength = this.stackB.length;
+
+      for(let i = 0; i < stackBLength; i++) {
+        let item = this.stackA.pop();
+        this.stackA.push(item);
       }
-      return this.stackB.pop();
     }
-  // enqueue(value) {
+    // console.log('this.stackA', this.stackA);
+      this.stackA.push(value);
+  }
 
-  //   if (this.stackB.length > 0){
-  //     let stackBLength = this.stackB.length;
+  dequeue() {
+    if(!this.stackA.top && !this.stackB.top){ return 'Queue is empty';}
 
-  //     for(let i = 0; i < stackBLength; i++) {
-  //       let item = this.stackA.pop();
-  //       this.stackA.push(item);
-  //     }
-  //   }
-  //   console.log('this.stackA', this.stackA);
-  //     this.stackA.push(value);
-  // }
-
-  // dequeue() {
-  //   if (this.stackB.length > 0) {
-  //     return this.stackB.pop();
-  //   } else if (this.stackB.length === 0) {
-  //       if(this.stackA.length === 0){
-  //         return "Que is empty";
-  //       } else if (this.stackA.length === 1){
-  //         return this.stackA.pop();
-  //       } else if (this.stackA.length > 0){
-  //         let stackALength = this.stackA.length;
-  //         for(let j = 0; j < stackALength; j++) {
-  //           let item2 = this.stackA.pop();
-  //           this.stackB.push(item2);
-  //         }
-  //         return this.stackB.pop();
-  //       }
-  //   }
-  // }
+    if (this.stackB.length > 0) {
+      return this.stackB.pop();
+    } else if (this.stackB.length === 0) {
+        if(this.stackA.length === 0){
+          return "Queue is empty";
+        } else if (this.stackA.length === 1){
+          return this.stackA.pop();
+        } else if (this.stackA.length > 0){
+          let stackALength = this.stackA.length;
+          for(let j = 0; j < stackALength; j++) {
+            let item2 = this.stackA.pop();
+            this.stackB.push(item2);
+          }
+          return this.stackB.pop();
+        }
+    }
+  }
 }
 
 module.exports = PseudoQueue;
