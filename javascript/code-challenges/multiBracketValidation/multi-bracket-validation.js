9995c872b80c39242e82f2307c3d08a992674151
Simon Panek <paneks19@gmail.com>
2020-12-23 20:41:55 -0800

    all tests passing

--- javascript/code-challenges/multiBracketValidation/multi-bracket-validation.js
+++ javascript/code-challenges/multiBracketValidation/multi-bracket-validation.js
@@ -1,82 +1,21 @@
 'use strict';
 
-// function multiFunctionValidate (string) {
-
-//   // let stringArray = Array.from(string);
-
-//   // let parenCount = 0;
-//   // let squareCount = 0;
-//   // let curlyCount = 0;
-
-//   // for (let i=0; i<stringArray.length; i++);
-//   //   if(stringArray[i] === '(') {parenCount ++};
-//   //   if(stringArray[i] === ')') {parenCount --};
-//   //   if(stringArray[i] === '[') {squareCount ++};
-//   //   if(stringArray[i] === ']') {squareCount --};
-//   //   if(stringArray[i] === '{') {curlyCount ++};
-//   //   if(stringArray[i] === '}') {curlyCount --};
-//   // }
-
-//   // if(parenCount === 0 && squareCount === 0 && curlyCount === 0){
-//   //   return true;
-//   // } else {
-//   //   return false;
-//   // }
-
-//   let stringArray = Array.from(string);
-
-//   if(!string) return false;
-
-//   let testArrayOne = [];
-// // console.log('1) string: ', string);
-//   for (let i = 0; i<stringArray.length; i++) {
-//     if(stringArray[i] === '(' || stringArray[i] === '{' || stringArray[i] === '('){
-//       testArrayOne.push(stringArray[i]);
-// // console.log('2) testArrayOne', testArrayOne);
-//       }
-//     if(stringArray[i] === ')' || stringArray[i] === '}' || stringArray[i] === '('){
-//     if(stringArray[i] === ')' && testArrayOne[testArrayOne.length - 1] !== '(') {
-// // console.log('3) testArrayOne', testArrayOne);
-//     return false;
-//     } else {
-// // console.log('4) testArrayOne', testArrayOne);
-// testArrayOne.pop();
-// // console.log('4.5) testArrayOne', testArrayOne);
-//     };
-//       if(stringArray[i] === '}' && testArrayOne[testArrayOne.length - 1] !== '{') {
-// // console.log('5) testArrayOne', testArrayOne);
-//       return false;
-//       } else {
-// // console.log('6) testArrayOne', testArrayOne);
-// testArrayOne.pop();
-// // console.log('6.5) testArrayOne', testArrayOne);
-//       };
-//       if(stringArray[i] === ']' && testArrayOne[testArrayOne.length - 1] !== '[') {
-// // console.log('7) testArrayOne', testArrayOne);
-//         return false;
-//       } else {
-// // console.log('8) testArrayOne', testArrayOne);
-//         testArrayOne.pop();
-//       };
-//     }
-// // console.log('9) testArrayOne', testArrayOne);
-//     return (testArrayOne === []) ? true : false;
-//   }
-
-// }
-
 function multiFunctionValidate (string) {
 
   let stringArray = Array.from(string);
   if(!string) return false;
 
   let testArrayOne = [];
+  let bracketCheck = false;
+
   for (let i = 0; i < stringArray.length; i++) {
     if(stringArray[i] === '(' || stringArray[i] === '{' || stringArray[i] === '['){
       testArrayOne.push(stringArray[i]);
+      bracketCheck = true;
       continue;
       }
     if(stringArray[i] === ')' || stringArray[i] === '}' || stringArray[i] === ']'){
+      bracketCheck = true;
       if((stringArray[i] === ')') && (testArrayOne[testArrayOne.length - 1] !== '(')) {
         return false;
       } else if (stringArray[i] === '}' && testArrayOne[testArrayOne.length - 1] !== '{') {
@@ -88,7 +27,7 @@ function multiFunctionValidate (string) {
       };
     }
   }
-  if(testArrayOne.length === 0) {
+  if(testArrayOne.length === 0 && bracketCheck === true) {
     return true;
   } else {
     return false;
