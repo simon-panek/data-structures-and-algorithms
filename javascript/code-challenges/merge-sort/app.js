d7141499fa326b7f7f29ef4276bbfb3d0899941b
Simon Panek <paneks19@gmail.com>
2021-01-12 15:44:55 -0800

    all tests passing

--- javascript/code-challenges/merge-sort/app.js
+++ javascript/code-challenges/merge-sort/app.js
@@ -1,58 +1,90 @@
 'use strict';
 
-function _mergeSort (array){
+// function _mergeSort (array){
 
-  console.log('starting array ', array);
+//   console.log('starting array ', array);
 
-  if(array.length <=1) return;
+//   if(array.length <=1) return;
 
-  let n = array.length;
+//   let n = array.length;
 
-  if (n>1){
-    let mid = Math.ceil(n/2);
-    let left = array.slice(0,mid);
-    let right = array.slice(mid, n);
-    console.log({left});
-    console.log({right});
+//   if (n>1){
+//     let mid = Math.ceil(n/2);
+//     let left = array.slice(0,mid);
+//     let right = array.slice(mid, n);
+//     console.log({left});
+//     console.log({right});
 
-    _mergeSort(left);
-    _mergeSort(right);
-    merge(left, right, array)
-    // merge(left, right)
+//     _mergeSort(left);
+//     _mergeSort(right);
+//     merge(left, right, array)
+//     // merge(left, right)
+//   }
+
+//   return array;
+// }
+
+// function merge (left, right, array) {
+// // function merge (left, right) {
+//   let i = 0;
+//   let j = 0;
+//   let k = 0;
+//   // let k = [];
+//   // let array = [];
+
+//   while (i < left.length && j < right.length) {
+//     if (left[i] <= right[j]){
+//       array[k] = left[i];
+//       i++;
+//     } else {
+//       array[k] = right[j];
+//       j++;
+//     }
+//     k++;
+//   }
+
+//   if (i === left.length){
+//     array.concat(left.slice[i])
+//   } else {
+//     array.concat(right.slice[j])
+//   }
+
+//   console.log({array});
+
+//   return array;
+
+// }
+
+// module.exports = _mergeSort;
+
+//Solution from Medium article below: https://medium.com/javascript-in-plain-english/javascript-merge-sort-3205891ac060
+
+function _mergeSort (array) {
+  if (array.length <= 1){
+    return array;
   }
+  const mid = Math.floor(array.length / 2);
+  const left = array.slice(0, mid);
+  const right = array.slice(mid);
 
-  return array;
+  return merge (_mergeSort(left), _mergeSort(right));
 }
 
-function merge (left, right, array) {
-// function merge (left, right) {
-  let i = 0;
-  let j = 0;
-  let k = 0;
-  // let k = [];
-  // let array = [];
+function merge(left, right) {
+  let outputArray = [];
+  let leftIndex = 0;
+  let rightIndex = 0;
 
-  while (i < left.length && j < right.length) {
-    if (left[i] <= right[j]){
-      array[k] = left[i];
-      i++;
+  while(leftIndex < left.length && rightIndex < right.length){
+    if (left[leftIndex] < right[rightIndex]){
+      outputArray.push(left[leftIndex]);
+      leftIndex++;
     } else {
-      array[k] = right[j];
-      j++;
+      outputArray.push(right[rightIndex]);
+      rightIndex++;
     }
-    k++;
   }
-
-  if (i === left.length){
-    array.concat(left.slice[i])
-  } else {
-    array.concat(right.slice[j])
-  }
-
-  console.log({array});
-
-  return array;
-
+  return outputArray.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
 }
 
 module.exports = _mergeSort;
--- javascript/code-challenges/merge-sort/merge-sort.test.js
+++ javascript/code-challenges/merge-sort/merge-sort.test.js
@@ -8,18 +8,18 @@ describe ('Merge Sort', () => {
     expect(_mergeSort(array)).toEqual([2,3,5,8,17,45,89,99]);
   })
 
-  // it('The merge sort function should not change an array that is already in order from lowest to highest', ()=> {
-  //   let array = [2,5,8,17,45,89];
-  //   expect(_mergeSort(array)).toEqual([2,5,8,17,45,89]);
-  // })
+  it('The merge sort function should not change an array that is already in order from lowest to highest', ()=> {
+    let array = [2,5,8,17,45,89];
+    expect(_mergeSort(array)).toEqual([2,5,8,17,45,89]);
+  })
 
-  // it('The merge sort function should finish sorting an array lowest to highest that is already partially sorted', ()=> {
-  //   let array = [2,5,8,45,17,89];
-  //   expect(_mergeSort(array)).toEqual([2,5,8,17,45,89]);
-  // })
+  it('The merge sort function should finish sorting an array lowest to highest that is already partially sorted', ()=> {
+    let array = [2,5,8,45,17,89];
+    expect(_mergeSort(array)).toEqual([2,5,8,17,45,89]);
+  })
 
-  // it('The merge sort function should sort an array lowest to highest that contains duplicate values', ()=> {
-  //   let array = [5,8,45,2,89,45,17];
-  //   expect(_mergeSort(array)).toEqual([2,5,8,17,45,45,89]);
-  // })
+  it('The merge sort function should sort an array lowest to highest that contains duplicate values', ()=> {
+    let array = [5,8,45,2,89,45,17];
+    expect(_mergeSort(array)).toEqual([2,5,8,17,45,45,89]);
+  })
 })
