a507183e42452e76b8827b520387beb8ae709c8b
Simon Panek <paneks19@gmail.com>
2021-01-19 13:47:18 -0800

    code written and functioning, one test passing

---
+++ javascript/code-challenges/tree-intersection/README.md
@@ -0,0 +1,14 @@
+# Challenge Summary
+
+## Authors: Simon Panek
+
+## Challenge Description
+
+A function that takes in two binary trees and returns an array of the non-duplicate values.
+
+## Approach & Efficiency
+<!-- What approach did you take? Why? What is the Big O space/time for this approach? -->
+
+## Solution
+
+![Whiteboard Solution]()
---
+++ javascript/code-challenges/tree-intersection/treeIntersection.js
@@ -0,0 +1,35 @@
+'use strict';
+const HashMap = require('../hashMap/hashMap.js');
+
+//create a new hashmap
+//go through each tree
+//for each tree node check if hashmap contains the number
+//if so continue
+//if not set into hashmap and push to output array
+//when all nodes visited return output array
+
+function treeIntersection (treeOne, treeTwo) {
+
+  if(!treeOne.root || !treeTwo.root) return null;
+
+  let map = new HashMap (100);
+  let outputArray = [];
+
+  function _treeTraverse (node) {
+    console.log('node.value ', node.value);
+    if(!map.contains(node.value.toString())){
+      map.set(node.value.toString());
+      outputArray.push(node.value);
+    }
+    if(node.leftChild) _treeTraverse(node.leftChild);
+    if(node.rightChild) _treeTraverse(node.rightChild);
+
+  }
+
+  _treeTraverse(treeOne.root);
+  _treeTraverse(treeTwo.root);
+
+  return outputArray;
+}
+
+module.exports = treeIntersection;
